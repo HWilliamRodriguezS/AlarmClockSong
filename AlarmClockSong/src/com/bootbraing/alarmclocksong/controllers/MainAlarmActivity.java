99f7cb1869c8ab950fff004f65990ee1a16106b4
HWilliamRodriguezS <hwilliamrs@outlook.com>
2014-08-28 11:34:05 -0400

    Problems and Bugs: 	-Adapt the new Alarms to Disable alarms in the alarm list 	-Fix the height and width distributions of the rows in the table, when you change the screen orientation.

--- AlarmClockSong/src/com/bootbraing/alarmclocksong/controllers/MainAlarmActivity.java
+++ AlarmClockSong/src/com/bootbraing/alarmclocksong/controllers/MainAlarmActivity.java
@@ -37,53 +37,63 @@ import com.bootbraing.alarmclocksong.models.Alarm.AlarmFormat;
 @SuppressLint("ClickableViewAccessibility")
 public class MainAlarmActivity extends Activity {
 
-	//	private final int MAIN_ALARM_ACTIVITY = 1;
+	// private final int MAIN_ALARM_ACTIVITY = 1;
 	private final int SET_ALARM_ACTIVITY = 2;
 	private List<Alarm> alarms = new ArrayList<Alarm>();
+	private boolean loaded;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_alarm);
-		findViewById(R.id.mainAlarmLayout).setOnTouchListener(new View.OnTouchListener() {
-			
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				v.setBackgroundResource(drawable.list_selector_background);
-				return false;
-			}
-		});
-		
+		findViewById(R.id.mainAlarmLayout).setOnTouchListener(
+				new View.OnTouchListener() {
+
+					@Override
+					public boolean onTouch(View v, MotionEvent event) {
+						v.setBackgroundResource(drawable.list_selector_background);
+						return false;
+					}
+				});
+		loaded = false;
+
 	}
-	
+
 	@Override
-	 public void onWindowFocusChanged(boolean hasFocus) {
-	  super.onWindowFocusChanged(hasFocus);
-	  listAllAlarms();
-	 }
-	
+	public void onWindowFocusChanged(boolean hasFocus) {
+		super.onWindowFocusChanged(hasFocus);
+		if (!loaded) {
+			listAllAlarms();
+			loaded = true;
+		}
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		listAllAlarms();
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_alarm, menu);
-		
-		
-		
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		
-		switch(item.getItemId()){
+
+		switch (item.getItemId()) {
 		default:
 		case R.id.action_settings:
-		Intent i = new Intent(this, AlarmPreferences.class);
-		startActivityForResult(i, 10);	
-		break;
-	
+			Intent i = new Intent(this, AlarmPreferences.class);
+			startActivityForResult(i, 10);
+			break;
+
 		}
-		
+
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -91,164 +101,200 @@ public class MainAlarmActivity extends Activity {
 		Intent i = new Intent(this, SetAlarmActivity.class);
 		startActivityForResult(i, SET_ALARM_ACTIVITY);
 	}
-	
-	public void editAlarm(Alarm alarm){
+
+	public void editAlarm(Alarm alarm) {
 		alarm.setAlarmFormat(AlarmFormat.HOUR_24);
-//		Log.e("Before PutExtra","Alarm : " + alarm);
 		Intent i = new Intent(this, SetAlarmActivity.class);
-        i.putExtra("Alarm", alarm);
+		i.putExtra("Alarm", alarm);
 		startActivityForResult(i, SET_ALARM_ACTIVITY);
-		
+
 	}
-	
+
 	@SuppressLint("RtlHardcoded")
 	public void listAllAlarms() {
 		alarms = new AlarmDAO(this).getAlarms();
 
-		TableLayout table = (TableLayout) MainAlarmActivity.this.findViewById(R.id.tAlarms);
-		table.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
-		
+		TableLayout table = (TableLayout) MainAlarmActivity.this
+				.findViewById(R.id.tAlarms);
+		table.removeAllViews();
+
+
+
+		// Determine density
+		DisplayMetrics screenMetrics = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(screenMetrics);
+		int density = screenMetrics.densityDpi;
+
+		// table.setLayoutParams(new
+		// LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT));
+
+		// TableLayout table1 = (TableLayout)
+		// MainAlarmActivity.this.findViewById(R.id.tAlarms1);
+
 		for (Alarm alrm : alarms) {
 			LinearLayout vLine = new LinearLayout(this);
 			vLine.setLayoutParams(new LayoutParams(1, LayoutParams.MATCH_PARENT));
 			vLine.setBackgroundResource(R.drawable.vertical_gradle);
-			
+
 			LinearLayout hLine = new LinearLayout(this);
-			hLine.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,1));
+			hLine.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, 1));
 			hLine.setBackgroundResource(R.drawable.horizontal_gradle);
 			TableRow row = new TableRow(this);
-			//TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
-			TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.MATCH_PARENT);
+			// TableRow.LayoutParams lp = new
+			// TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT);
+			TableRow.LayoutParams lp = new TableRow.LayoutParams(
+					TableRow.LayoutParams.MATCH_PARENT,
+					TableRow.LayoutParams.MATCH_PARENT);
 			row.setLayoutParams(lp);
 			row.setId((int) alrm.getId());
-			
+
 			ToggleButton tbOnOff = new ToggleButton(this);
 			tbOnOff.setChecked(alrm.isEnabled());
 			tbOnOff.setBackgroundColor(Color.TRANSPARENT);
-			//tbOnOff.setBackgroundResource(R.layout.alarm_on_off);
+			// tbOnOff.setBackgroundResource(R.layout.alarm_on_off);
 			tbOnOff.setButtonDrawable(R.layout.alarm_on_off);
 			tbOnOff.setTextOff("");
 			tbOnOff.setTextOn("");
 			tbOnOff.setText("");
-			
-			tbOnOff.setWidth(table.getWidth()/4);
-			tbOnOff.setHeight(table.getWidth()/4);
-			//tbOnOff.setPadding(4, 4, 4, 4);
+
+			tbOnOff.setWidth(density / 2);
+			tbOnOff.setHeight(density / 2);
+			// tbOnOff.setPadding(4, 4, 4, 4);
 			tbOnOff.setGravity(Gravity.CENTER);
-			//tbOnOff.setG
-            //tbOnOff.
+			// tbOnOff.setG
+			// tbOnOff.
 			tbOnOff.buildDrawingCache(true);
-			
-		    //LayoutParams tbParams = new LayoutParams();
-			//tbParams.setMargins(24,24, 24, 24);
-			
-			//tbParams.
-			//tbOnOff.setLayoutParams(tbParams);
+
+			// LayoutParams tbParams = new LayoutParams();
+			// tbParams.setMargins(24,24, 24, 24);
+
+			// tbParams.
+			// tbOnOff.setLayoutParams(tbParams);
 
 			tbOnOff.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
 				public void onCheckedChanged(CompoundButton buttonView,
 						boolean isChecked) {
-					buttonView.setBackgroundResource(drawable.list_selector_background);
+					buttonView
+							.setBackgroundResource(drawable.list_selector_background);
 					TableRow tr = (TableRow) buttonView.getParent();
-					Alarm alarm = new AlarmDAO(getApplicationContext()).getAlarm(tr.getId());
+					Alarm alarm = new AlarmDAO(getApplicationContext())
+							.getAlarm(tr.getId());
 					OnOffAlarm onOffAlarm = new OnOffAlarm();
-	
+
 					if (isChecked) {
 						alarm.setEnabled(true);
-						onOffAlarm.turnAlarmOffOn(alarm, true,getApplicationContext());
+						onOffAlarm.turnAlarmOffOn(alarm, true,
+								getApplicationContext());
 					} else {
 						alarm.setEnabled(false);
-						onOffAlarm.turnAlarmOffOn(alarm, false,getApplicationContext());
+						onOffAlarm.turnAlarmOffOn(alarm, false,
+								getApplicationContext());
 					}
 					new AlarmDAO(getApplicationContext()).update(alarm);
-					
+
 				}
 			});
-			
+
 			alrm.setAlarmFormat(AlarmFormat.HOUR_24);
-			Log.d("Alarm Details" , " Alarm : " + alrm.toString());
-			Log.d("Alarm Repeating" , "Days : " + Integer.toBinaryString(alrm.getDaysOfWeek().getCoded()));
-			Log.d("=====","======");
-		
-			
+			Log.d("Alarm Details", " Alarm : " + alrm.toString());
+			Log.d("Alarm Repeating",
+					"Days : "
+							+ Integer.toBinaryString(alrm.getDaysOfWeek()
+									.getCoded()));
+			Log.d("=====", "======");
+
 			TableLayout tbr = new TableLayout(this);
-			LayoutParams tableParam = new LayoutParams((table.getWidth()/4)*3,LayoutParams.MATCH_PARENT);
+			LayoutParams tableParam = new LayoutParams(
+					(table.getWidth() / 4) * 3, LayoutParams.MATCH_PARENT);
 
 			tbr.setWeightSum(1);
 			tbr.setLayoutParams(tableParam);
 			tbr.setGravity(Gravity.CENTER);
 
 			TableRow row1_1 = new TableRow(this);
-			//Log.e("TableLayout " , "Width : " + tbr.getWidth());
+			// Log.e("TableLayout " , "Width : " + tbr.getWidth());
 			TextView tvAlarmTitle = new TextView(this);
 			tvAlarmTitle.setText("" + alrm.getLabel());
 			tvAlarmTitle.setGravity(Gravity.CENTER);
 			tvAlarmTitle.setTypeface(Typeface.DEFAULT_BOLD);
-			//InputFilter[] filterArray = new InputFilter[1];
-			//filterArray[0] = new InputFilter.LengthFilter(36);
-			//tvAlarmTitle.setFilters(filterArray);
+			// InputFilter[] filterArray = new InputFilter[1];
+			// filterArray[0] = new InputFilter.LengthFilter(36);
+			// tvAlarmTitle.setFilters(filterArray);
 			tvAlarmTitle.setEllipsize(TruncateAt.END);
-			
-			LayoutParams attParam = new LayoutParams(table.getWidth()/2,LayoutParams.MATCH_PARENT,0.7f);
+
+			LayoutParams attParam = new LayoutParams(table.getWidth() / 2,
+					LayoutParams.MATCH_PARENT, 0.7f);
 			attParam.gravity = Gravity.LEFT;
-			//tvAlarmTitle.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT,(0.7f)));
-			tvAlarmTitle.setLayoutParams(new LayoutParams(table.getWidth()/4,LayoutParams.MATCH_PARENT,(0.7f)));
-			//.setLayoutParams(new LayoutParams())
-			
+			// tvAlarmTitle.setLayoutParams(new
+			// LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT,(0.7f)));
+			tvAlarmTitle.setLayoutParams(new LayoutParams(table.getWidth() / 4,
+					LayoutParams.MATCH_PARENT, (0.7f)));
+			// .setLayoutParams(new LayoutParams())
+
 			TextView tvAlarmTime = new TextView(this);
-			//tvAlarmTime.setTextSize(22f);
+			// tvAlarmTime.setTextSize(22f);
 			DisplayMetrics metrics = new DisplayMetrics();
 			getWindowManager().getDefaultDisplay().getMetrics(metrics);
-			
-//			Log.e("Font Metrics : " , "Font Size :" + metrics.scaledDensity);
-//			Log.e("Font Metrics : " , "Font Size :" + metrics.DENSITY_MEDIUM);
-			
+
+			// Log.e("Font Metrics : " , "Font Size :" + metrics.scaledDensity);
+			// Log.e("Font Metrics : " , "Font Size :" +
+			// metrics.DENSITY_MEDIUM);
+
 			tvAlarmTime.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24);
-			//tvAlarmTime.setTextSize(metrics.scaledDensity);
+			// tvAlarmTime.setTextSize(metrics.scaledDensity);
 			tvAlarmTime.setTypeface(Typeface.DEFAULT_BOLD);
 			tvAlarmTime.setGravity(Gravity.CENTER);
-			//Log.e("Alarm Data : "," Alarm : " + alrm.toString());
+			// Log.e("Alarm Data : "," Alarm : " + alrm.toString());
 			tvAlarmTime.setText("" + alrm.getTimeStr());
-		    LayoutParams atParam = new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT,0.3f);
-		    atParam.gravity = Gravity.RIGHT;
-			//tvAlarmTime.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT,0.3f));
-			tvAlarmTime.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT,0.3f));
-			
+			LayoutParams atParam = new LayoutParams(LayoutParams.MATCH_PARENT,
+					LayoutParams.MATCH_PARENT, 0.3f);
+			atParam.gravity = Gravity.RIGHT;
+			// tvAlarmTime.setLayoutParams(new
+			// LayoutParams(LayoutParams.MATCH_PARENT,LayoutParams.MATCH_PARENT,0.3f));
+			tvAlarmTime
+					.setLayoutParams(new LayoutParams(
+							LayoutParams.MATCH_PARENT,
+							LayoutParams.MATCH_PARENT, 0.3f));
+
 			row1_1.addView(tvAlarmTitle);
 			row1_1.addView(tvAlarmTime);
-			
+
 			TableRow row1_2 = new TableRow(this);
-			
+
 			TextView tvAlarmDays = new TextView(this);
-			tvAlarmDays.setText(alrm.getDaysOfWeek().toString(getApplicationContext(), false));
-			
-			LayoutParams rowParam = new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT,1f);
+			tvAlarmDays.setText(alrm.getDaysOfWeek().toString(
+					getApplicationContext(), false));
+
+			LayoutParams rowParam = new LayoutParams(LayoutParams.WRAP_CONTENT,
+					LayoutParams.MATCH_PARENT, 1f);
 			rowParam.span = 2;
 			tvAlarmDays.setLayoutParams(rowParam);
-			
+
 			row1_2.addView(tvAlarmDays);
-			
+
 			tbr.addView(row1_1);
 			tbr.addView(row1_2);
-						
+
 			row.addView(tbr);
 			row.addView(vLine);
 			row.addView(tbOnOff);
 			row.setOnTouchListener(new View.OnTouchListener() {
-				
+
 				@Override
 				public boolean onTouch(View v, MotionEvent event) {
-					((TableRow)v).setBackgroundResource(drawable.list_selector_background);
+					((TableRow) v)
+							.setBackgroundResource(drawable.list_selector_background);
 					return false;
 				}
-				
+
 			});
-			
+
 			row.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					editAlarm(new AlarmDAO(getApplicationContext()).getAlarm(((TableRow)v).getId()));
+					editAlarm(new AlarmDAO(getApplicationContext())
+							.getAlarm(((TableRow) v).getId()));
 				}
 			});
 			table.addView(row);
@@ -256,12 +302,6 @@ public class MainAlarmActivity extends Activity {
 
 		}
 
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		this.onCreate(null);
 	}
 
 }
--- AlarmClockSong/src/com/bootbraing/alarmclocksong/controllers/OnOffAlarm.java
+++ AlarmClockSong/src/com/bootbraing/alarmclocksong/controllers/OnOffAlarm.java
@@ -1,29 +1,51 @@
 package com.bootbraing.alarmclocksong.controllers;
 
+import java.util.List;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 
 import com.bootbraing.alarmclocksong.models.Alarm;
 
-public class OnOffAlarm  {
-	
-	public void changeAlarmState(){}
-	
-	public void turnAlarmOffOn(Alarm alarm, boolean offOn,Context context){
-		AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+public class OnOffAlarm {
 
-		Intent intent = new Intent(context, AlarmReceiverActivity.class);
-		PendingIntent alarmIntent = PendingIntent.getActivity(context, Integer.parseInt(alarm.getHour() + "" + alarm.getMinutes()), intent,
-				PendingIntent.FLAG_CANCEL_CURRENT);
-
-		alarmManager.cancel(alarmIntent);
-		Log.d("","REMOVE ALARM :  " + alarm.toString());
-		
+	public void changeAlarmState() {
 	}
-	
-	public void setOnStateChangeListener(){}
+
+	public void turnAlarmOffOn(Alarm alarm, boolean offOn, Context context) {
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		/*
+		 * Intent intent = new Intent(context, AlarmReceiverActivity.class);
+		 * PendingIntent alarmIntent = PendingIntent.getActivity(context,
+		 * Integer.parseInt(alarm.getHour() + "" + alarm.getMinutes()), intent,
+		 * PendingIntent.FLAG_CANCEL_CURRENT);
+		 * 
+		 * alarmManager.cancel(alarmIntent);
+		 */
+
+		Alarms alarms = new Alarms(context);
+
+		if (offOn) {
+			alarms.setAlarm(context, alarm);
+		} else {
+			List<PendingIntent> pIntents = alarms.alarmPendingItents(alarm);
+			try {
+				for (PendingIntent pIntent : pIntents) {
+					alarmManager.cancel(pIntent);
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			Log.d("", "REMOVE ALARM :  " + alarm.toString());
+		}
+	}
+
+	public void setOnStateChangeListener() {
+	}
 
 }
